e6250108fe8b7d7faf66c8e4340724722ae6a7b6
Carl Tablante <cwtablante@sephora.sg>
2020-04-13 21:34:01 +0800

    Connected to postgres

--- main.go
+++ main.go
@@ -1,8 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+const (
+	db_name  = "werewolf"
+	host     = "localhost"
+	user     = "postgres"
+	password = ""
+	port     = 5432
+)
 
 func main() {
-	fmt.Println("vim-go")
-	//
+
+	dbinfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host,
+		port,
+		user,
+		password,
+		db_name,
+	)
+
+	db, err := sqlx.Open("postgres", dbinfo)
+	defer db.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Postgres connected")
 }
