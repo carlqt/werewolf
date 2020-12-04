a5c79a296eb37fb0f22452e81ae446db873b1668
cwtablante <carlwilliam.tablante@gmail.com>
2020-12-04 21:56:52 +0800

    Installed pgweb

--- models/root.go
+++ models/root.go
@@ -12,17 +12,18 @@ const (
 	dbName   = "werewolf"
 	host     = "localhost"
 	user     = "postgres"
-	password = ""
+	password = "postgres"
 	dbPort   = 5433
 )
 
 func InitDB() *sqlx.DB {
 	var err error
 
-	dbinfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
+	dbinfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host,
 		dbPort,
 		user,
+		password,
 		dbName,
 	)
 
